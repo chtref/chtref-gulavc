d92d191eabd3c49f543bafa9c4b5f7597d805d7a
Guillaume Lavoie <guillaume.laboie-harvey@polymtl.ca>
2016-10-19 18:49:06 -0400

    Slightly less bad, jai utilise les notes de cours pour cet algorithme plutot que celui de l'enonce

--- INF4710 (Multimedia)/inf4710_tp2_cpp/dct.h
+++ INF4710 (Multimedia)/inf4710_tp2_cpp/dct.h
@@ -12,27 +12,28 @@ inline cv::Mat_<Tout> dct(const cv::Mat_<Tin>& oBlock) {
     // @@@@ TODO
 	int n = oBlock.rows;
 
-	for (int u = 0; u < oOutput.rows; u++) {
-		for (int v = 0; v < oOutput.cols; v++) {
+	for (int u = 0; u < oOutput.rows; ++u) {
+		for (int v = 0; v < oOutput.cols; ++v) {
 
 			float result = 0.0;
-			for (int i = 1; i <= n; i++) {
-				for (int j = 1; j <= n; j++) {
+
+			for (int x = 0; x < (n - 1); ++x) {
+				for (int y = 0; y < (n - 1); ++y) {
 					
 					// F_xy * cos#1 * cos#2
-					result += oBlock.at<uchar>(i-1, j-1) *cos((CV_PI * (2.0 * (i - 1.0) + 1.0) * (u - 1.0) / (2.0 * n))) * cos((CV_PI * (2.0 * (j - 1.0) + 1.0) * (v - 1.0) / (2.0 * n)));
+					result += float(oBlock.at<uchar>(x, y)) * cos(((CV_PI * (2.0*(x) + 1.0)*(u)) / (2.0*n))) * cos(((CV_PI * (2.0*(y) + 1.0)*(v)) / (2.0*n)));
 
 				}
 			}
 			float c_u, c_v;
-			if (u == 1) {
+			if (u == 0) {
 				c_u = sqrt(1.0 / n);
 			}
 			else {
 				c_u = sqrt(2.0 / n);
 			}
 
-			if (v == 1) {
+			if (v == 0) {
 				c_v = sqrt(1.0 / n);
 			}
 			else {
--- INF4710 (Multimedia)/inf4710_tp2_cpp/dct_inv.h
+++ INF4710 (Multimedia)/inf4710_tp2_cpp/dct_inv.h
@@ -12,22 +12,23 @@ inline cv::Mat_<Tout> dct_inv(const cv::Mat_<Tin>& oBlock) {
 	// @@@@ TODO
 	int n = oBlock.rows;
 
-	for (int i = 0; i < oOutput.rows; i++) {
-		for (int j = 0; j < oOutput.cols; j++) {
+	for (int x = 0; x < oOutput.rows; ++x) {
+		for (int y = 0; y < oOutput.cols; ++y) {
 
 			float F_xy = 0.0;
-			for (int u = 1; u <= n; u++) {
-				for (int v = 1; v <= n; v++) {
+
+			for (int u = 0; u < (n - 1); ++u) {
+				for (int v = 0; v < (n - 1); ++v) {
 
 					float c_u, c_v;
-					if (u == 1) {
+					if (u == 0) {
 						c_u = sqrt(1.0 / n);
 					}
 					else {
 						c_u = sqrt(2.0 / n);
 					}
 
-					if (v == 1) {
+					if (v == 0) {
 						c_v = sqrt(1.0 / n);
 					}
 					else {
@@ -35,13 +36,10 @@ inline cv::Mat_<Tout> dct_inv(const cv::Mat_<Tin>& oBlock) {
 					}
 
 					// C_uv * cos#1 * cos#2
-					F_xy += c_u * c_v * oBlock.at<float>(u-1, v-1) *cos((CV_PI * (2.0 * (i - 1.0) + 1.0) * (u - 1.0) / (2.0 * n))) * cos((CV_PI * (2.0 * (j - 1.0) + 1.0) * (v - 1.0) / (2.0 * n)));
-
+					F_xy += c_u * c_v * oBlock.at<float>(u, v) * cos(((CV_PI * (2.0*(x) + 1.0)*(u)) / (2.0*n))) * cos(((CV_PI * (2.0*(y) + 1.0)*(v)) / (2.0*n)));
 				}
 			}
-			
-
-			oOutput.at<uchar>(i, j) = cv::saturate_cast<uchar>(F_xy);
+			oOutput.at<uchar>(x, y) = cv::saturate_cast<uchar>(F_xy);
 		}
 	}
 
