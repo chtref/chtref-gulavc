51c73684b41cc905bc5b67deb8bdb9881d85d1c5
Guillaume Lavoie <guillaume.laboie-harvey@polymtl.ca>
2016-11-02 18:33:31 -0400

    filtres ideaux + distortion funhouse

--- INF8725 (Signaux)/tp2/tp2.m
+++ INF8725 (Signaux)/tp2/tp2.m
@@ -118,10 +118,10 @@ imshow(img_o)
 %Exercice 3.2
 
 fft2_h = fft2(img_h,size(img_h,1),size(img_h,2));
-fft2_h = abs(fft2_h/sum(sum(fft2_h)));
+fft2_h = abs(fft2_h/numel(fft2_h));
 
 fft2shift_h = fftshift(fft2_h);
-fft2shift_h = 1 + log(fft2shift_h);
+fft2shift_h = log(1 + fft2shift_h);
 
 figure(9)
 subplot(221)
@@ -129,20 +129,20 @@ imshow(fft2shift_h,[])
 
 %
 fft2_v = fft2(img_v,size(img_v,1),size(img_v,2));
-fft2_v = abs(fft2_v/sum(sum(fft2_v)));
+fft2_v = abs(fft2_v/numel(fft2_v));
 
 fft2shift_v = fftshift(fft2_v);
-fft2shift_v = 1 + log(fft2shift_v);
+fft2shift_v = log(1 + fft2shift_v);
 
 subplot(222)
 imshow(fft2shift_v,[])
 
 %
 fft2_o = fft2(img_o,size(img_o,1),size(img_o,2));
-fft2_o = abs(fft2_o/sum(sum(fft2_o)));
+fft2_o = abs(fft2_o/numel(fft2_o));
 
 fft2shift_o = fftshift(fft2_o);
-fft2shift_o = 1 + log(fft2shift_o);
+fft2shift_o = log(1 + fft2shift_o);
 
 subplot(223)
 imshow(fft2shift_o,[])
@@ -158,10 +158,10 @@ subplot(224)
 imshow(img_v_rotate)
 
 fft2_v_rot = fft2(img_v_rotate,size(img_v_rotate,1),size(img_v_rotate,2));
-fft2_v_rot = abs(fft2_v_rot/sum(sum(fft2_v_rot)));
+fft2_v_rot = abs(fft2_v_rot/numel(fft2_v_rot));
 
 fft2shift_v_r = fftshift(fft2_v_rot);
-fft2shift_v_r = 1 + log(fft2shift_v_r);
+fft2shift_v_r = log(1 + fft2shift_v_r);
 
 figure(9)
 subplot(224)
@@ -170,10 +170,63 @@ imshow(fft2shift_v_r,[])
 %%
 %Exercice 3.4
 
-%
+% TODO
 
 %%
 %Exercice 4
 
 %%
 %Exercice 4.1
+
+img_house = imread('NewHouse.png');
+img_house_gray = rgb2gray(img_house);
+figure(10)
+subplot(121)
+imshow(img_house_gray,[])
+
+fft2_house = fft2(img_house_gray);
+fft2_house = fftshift(fft2_house/numel(fft2_house));
+
+
+subplot(122)
+imshow(abs(fft2_house))
+
+%%
+%Exercice 4.2
+
+schema = imread('schema_colored.png');
+figure(11)
+imshow(schema)
+
+% Les différentes couleurs du schéma représentes les sections suivantes de
+% la maison:
+% Blue: Roof
+% Green: Wall
+% Yellow: Left Window
+% Orange: Chimney
+% Red: Door
+% Purple: Right Window
+
+%%
+%Exercice 4.3
+
+H = fspecial('gaussian', size(img_house_gray), 12);
+filtre_toit = imfilter(img_house_gray,H);
+
+figure(12)
+imshow(filtre_toit)
+
+%%
+%Exercice 4.4
+
+ideal_filter = double(rgb2gray(imread('ideal_filter.png')));
+ideal_filter = ideal_filter/sum(sum(ideal_filter));
+ideal_filter = imresize(ideal_filter,[825 936]);
+
+filtre_windows = fft2_house .* ideal_filter;
+
+filtered_img = ifftshift(filtre_windows);
+filtered_img = ifft2(filtered_img);
+
+figure(13)
+imshow(abs(filtered_img),[])
